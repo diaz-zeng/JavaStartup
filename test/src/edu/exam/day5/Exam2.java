9de09e9939737f24439724e087608e8d4cf51bc4
Diaz <zly981026@outlook.com>
2018-09-24 23:58:38 +0800

    Bubble fixed

--- test/src/edu/exam/day5/Exam2.java
+++ test/src/edu/exam/day5/Exam2.java
@@ -98,14 +98,14 @@ public class Exam2
 
 		for (int i = 0; i < arr.length; i++)
 		{
-			for (int j = i + 1; j < arr.length; j++)
+			for (int j = 1; j < arr.length-i-1; j++)
 			{
 				int temp = 0;
-				if (arr[i] > arr[j])
+				if (arr[j-1] > arr[j])
 				{
-					temp = arr[j];
-					arr[j] = arr[i];
-					arr[i] = temp;
+					temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
 				}
 			}
 		}

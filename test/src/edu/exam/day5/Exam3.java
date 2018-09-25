ed11823dc0d8cd8bc6d7b42fc6281b7c8875a0b7
Diaz <zly981026@outlook.com>
2018-09-25 14:17:00 +0800

    day5.Exam3 end

--- test/src/edu/exam/day5/Exam3.java
+++ test/src/edu/exam/day5/Exam3.java
@@ -18,14 +18,14 @@ public class Exam3
 			System.out.print(i+"\t");
 		}
 		System.out.println();
-//		for(int i = 0;i<arr2.length;i++)
-//		{
-//			if(i%2==0)
-//			{
-//				arr2[i]=i;
-//			}
-//			System.out.print(arr1[i]+"\t");
-//		}
+		for(int i = 0;i<arr2.length;i++)
+		{
+			if(i%2==0)
+			{
+				arr2[i]=i;
+			}
+			System.out.print(arr1[i]+"\t");
+		}
 		java.util.Arrays.toString(arr1);
 	}
 
